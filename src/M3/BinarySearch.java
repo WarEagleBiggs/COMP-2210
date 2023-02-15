f4be831990dca246e87235752d7921b0273b0c4f
Adam Biggs <wareaglebiggs@gmail.com>
2023-02-15 09:20:40 -0600

    New files and moved files

---
+++ src/M3/BinarySearch.java
@@ -0,0 +1,29 @@
+package M3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Binary search.
+ */
+public class BinarySearch {
+
+    /**
+     * Returns the index of the first key in a[] that equals the search key, 
+     * or -1 if no such key exists. This method throws a NullPointerException
+     * if any parameter is null.
+     */
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+    return -99;
+    }
+
+    /**
+     * Returns the index of the last key in a[] that equals the search key, 
+     * or -1 if no such key exists. This method throws a NullPointerException
+     * if any parameter is null.
+     */
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+    return -99;
+    }
+
+}
---
+++ src/M3/Term.java
@@ -0,0 +1,56 @@
+package M3;
+
+import java.util.Comparator;
+
+/**
+ * Autocomplete term representing a (query, weight) pair.
+ * 
+ */
+public class Term implements Comparable<Term> {
+
+    /**
+     * Initialize a term with the given query and weight.
+     * This method throws a NullPointerException if query is null,
+     * and an IllegalArgumentException if weight is negative.
+     */
+    public Term(String query, long weight) {
+    
+    }
+
+    /**
+     * Compares the two terms in descending order of weight.
+     */
+    public static Comparator<Term> byDescendingWeightOrder() {
+        return null;
+    }
+
+    /**
+     * Compares the two terms in ascending lexicographic order of query,
+     * but using only the first length characters of query. This method
+     * throws an IllegalArgumentException if length is less than or equal
+     * to zero.
+     */
+    public static Comparator<Term> byPrefixOrder(int length) {
+        return null;
+    }
+
+    /**
+     * Compares this term with the other term in ascending lexicographic order
+     * of query.
+     */
+    @Override
+    public int compareTo(Term other) {
+        return -99;
+    }
+
+    /**
+     * Returns a string representation of this term in the following format:
+     * query followed by a tab followed by weight
+     */
+    @Override
+    public String toString(){
+        return null;
+    }
+
+}
+
