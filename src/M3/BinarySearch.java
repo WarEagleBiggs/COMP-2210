1823faf1989d383b7ac2bd0717cb8fcffd5544d9
Adam Biggs <wareaglebiggs@gmail.com>
2023-02-25 19:17:32 -0600

    Binary search

--- src/M3/BinarySearch.java
+++ src/M3/BinarySearch.java
@@ -14,7 +14,38 @@ public class BinarySearch {
      * if any parameter is null.
      */
     public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-    return -99;
+
+        //throw
+        if (a == null || key == null || comparator == null) {
+            throw new NullPointerException("null");
+        }
+
+        int base = 0;
+        int top = a.length - 1;
+
+        if (comparator.compare(a[0], key) == 0) {
+            return 0;
+        }
+
+        while (base <= top)
+        {
+            //get the middle
+            int mean = base + (top - base) / 2;
+
+            if (comparator.compare(key, a[mean]) < 0) {
+                top = mean - 1;
+            }
+            else if (comparator.compare(key, a[mean]) > 0) {
+                base = mean + 1;
+            }
+            else if (comparator.compare(a[mean - 1], a[mean]) == 0) {
+                top = mean - 1;
+            }
+
+            else return mean;
+        }
+
+        return -1;
     }
 
     /**
@@ -23,7 +54,39 @@ public class BinarySearch {
      * if any parameter is null.
      */
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-    return -99;
+
+        //throw
+        if (a == null || key == null || comparator == null) {
+            throw new NullPointerException("null");
+        }
+
+        int base = 0;
+        int top = a.length - 1;
+
+        if (comparator.compare(a[top], key) == 0) {
+
+            return top;
+
+        }
+
+        while (base <= top)
+        {
+            //middle
+            int mean = base + (top - base) / 2;
+
+            if (comparator.compare(key, a[mean]) < 0) {
+                top = mean - 1;
+            }
+            else if (comparator.compare(key, a[mean]) > 0) {
+                base = mean + 1;
+            }
+            else if (comparator.compare(a[mean + 1], a[mean]) == 0) {
+                base = mean + 1;
+            }
+
+            else return mean;
+        }
+        return -1;
     }
 
 }
